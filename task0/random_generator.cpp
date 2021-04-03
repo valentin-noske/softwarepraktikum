6c541d32e09ff7a75d2e837836589d578b3bbaa6
valentin-noske <valentin@schaefer-noske.de>
2021-04-03 19:55:58 +0200

    Random ints

--- task0/random_generator.cpp
+++ task0/random_generator.cpp
@@ -1,6 +1,29 @@
 //
 // Created by valentin on 03.04.21.
 //
+#include "random_generator.hpp"
+
+std::vector<int> key_numbers(int count) {
+    std::vector<int> numbers = {};
+    for(uint counter = 0; counter < count; ++counter) {
+            numbers.push_back(counter);
+    }
+    return numbers;
+}
+std::string get_random_string(){
+    
+}
+
+std::vector<std::string> value_strings(int count) {
+    std::vector<string> strings = {};
+    for(uint counter = 0; counter < count; ++counter) {
+        numbers.push_back(counter);
+    }
+    return numbers;
+}
+
+
+
 
 
 
